3ac1eda2a4dbb5cc5b5ff4cf3942d9dac08d19d3
Bora Mac <bora.deren@rwth-aachen.de>
2025-10-12 21:15:44 +0200

    Relocate log files to `logs/` directory and update file references accordingly.

--- fun.py
+++ fun.py
@@ -1,7 +1,7 @@
 import pm4py
 import pm4py_ext
 
-log = pm4py.read_xes('event-log.xes')
+log = pm4py.read_xes('logs/event-log.xes')
 
 dfg, start_activities, end_activities = pm4py.discover_dfg(log)
 filtered_dfg, filtered_start, filtered_end = pm4py.filter_dfg_activities_percentage(
